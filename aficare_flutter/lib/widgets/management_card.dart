aad32640f4e23e388bbb46b505ec909e24dc6a15
Winfry <Winfrynyarangi@gmail.com>
2026-07-27 12:07:45 +0300

    ManagementCard: add hover animation (StatefulWidget)

--- aficare_flutter/lib/widgets/management_card.dart
+++ aficare_flutter/lib/widgets/management_card.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import '../theme/app_colors.dart';
 
-class ManagementCard extends StatelessWidget {
+class ManagementCard extends StatefulWidget {
   const ManagementCard({
     super.key,
     required this.icon,
@@ -20,50 +20,61 @@ class ManagementCard extends StatelessWidget {
   final VoidCallback? onTap;
 
   @override
+  State<ManagementCard> createState() => _ManagementCardState();
+}
+
+class _ManagementCardState extends State<ManagementCard> {
+  bool _hovering = false;
+
+  @override
   Widget build(BuildContext context) {
-    return Material(
-      color: Colors.transparent,
-      child: InkWell(
-        borderRadius: BorderRadius.circular(16),
-        onTap: onTap,
-        child: Container(
-          padding: const EdgeInsets.all(18),
-          decoration: BoxDecoration(
-            color: Colors.white,
+    return MouseRegion(
+      onEnter: (_) => setState(() => _hovering = true),
+      onExit: (_) => setState(() => _hovering = false),
+      cursor: SystemMouseCursors.click,
+      child: AnimatedContainer(
+        duration: const Duration(milliseconds: 150),
+        transform: Matrix4.translationValues(0, _hovering ? -2 : 0, 0),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(16),
+          border: Border.all(color: _hovering ? Colors.transparent : AppColors.borderSubtle),
+          boxShadow: _hovering
+              ? [BoxShadow(color: AppColors.deepNavy.withOpacity(.14), blurRadius: 18, offset: const Offset(0, 6))]
+              : [],
+        ),
+        child: Material(
+          color: Colors.transparent,
+          child: InkWell(
             borderRadius: BorderRadius.circular(16),
-            border: Border.all(color: AppColors.borderSubtle),
-          ),
-          child: Row(
-            children: [
-              Container(
-                width: 38,
-                height: 38,
-                alignment: Alignment.center,
-                decoration: BoxDecoration(
-                  color: iconBackground,
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                child: Icon(icon, size: 18, color: iconColor),
+            onTap: widget.onTap,
+            child: Padding(
+              padding: const EdgeInsets.all(20),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                    width: 38,
+                    height: 38,
+                    alignment: Alignment.center,
+                    decoration: BoxDecoration(color: widget.iconBackground, borderRadius: BorderRadius.circular(10)),
+                    child: Icon(widget.icon, size: 17, color: widget.iconColor),
+                  ),
+                  const SizedBox(width: 14),
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(widget.title, style: const TextStyle(fontSize: 14, fontWeight: FontWeight.w600)),
+                        const SizedBox(height: 4),
+                        Text(widget.description,
+                            style: const TextStyle(fontSize: 12, color: AppColors.textMuted, height: 1.5)),
+                      ],
+                    ),
+                  ),
+                ],
               ),
-              const SizedBox(width: 14),
-              Expanded(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(title,
-                        style: const TextStyle(fontSize: 13.5, fontWeight: FontWeight.w600)),
-                    const SizedBox(height: 2),
-                    Text(description,
-                        style: const TextStyle(fontSize: 11.5, color: AppColors.textMuted),
-                        maxLines: 1,
-                        overflow: TextOverflow.ellipsis),
-                  ],
-                ),
-              ),
-              const SizedBox(width: 8),
-              Icon(Icons.chevron_right_rounded, size: 20, color: AppColors.textMuted),
-            ],
+            ),
           ),
         ),
       ),
